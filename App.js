7e53e20cf52cea6e6a6ce9020c54fa7bb87be817
Rodolphe <dutchr@live.fr>
2022-01-03 14:56:41 +0100

    :sparkles: Adding submit button & setState datas

--- App.js
+++ App.js
@@ -1,60 +1,62 @@
-import React, { Component } from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
-import Heading from './components/Heading'
-import Input from './components/TextInput'
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import Button from "./components/Button";
+import Heading from "./components/Heading";
+import Input from "./components/TextInput";
 
-let todoIndex = 0
+let todoIndex = 0;
 export default class App extends Component {
   state = {
-  inputValue: '',
-  todos:[],
-  type: 'All'
-}
-inputChange(inputValue) {
-  console.log(' Input value: ', inputValue)
-  this.setState({inputValue})
-}
+      inputValue: "",
+      todos: [],
+      type: "All",
+    };
+  
+  inputChange(inputValue) {
+    console.log(" Input value: ", inputValue);
+    this.setState({ inputValue });
+  }
 
-submitTodo(){
-  if(this.state.inputValue.match(/^\d+$/)) {
-    return 
+   submitTodo =()=> {
+    if (this.state.inputValue.match(/^\d+$/)) {
+      return
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false,
+    };
+    todoIndex++;
+    const todos = [...this.state.todos, todo];
+    this.setState({ todos, inputValue: "" }, () => {
+      console.log("State: ", this.state);
+    });
   }
-  const todo = {
-    title: this.state.inputValue,
-    todoIndex,
-    complete:false
-  }
-  todoIndex++
-  const todos = [...this.state.todos, todo]
-  this.setState({todos, inputValue: ''}, ()=> { console.log('State: ', this.state)})
-}
 
   render() {
-    const { inputValue} = this.state
+    let { inputValue } = this.state;
     return (
-      <View style ={styles.container}>
-        <ScrollView style={styles.content}> 
-        <Heading />
-        <Input 
-        inputValue={inputValue}
-        inputChange={(text)=> this.inputChange(text)}
-        />
-         </ScrollView>
+      <View style={styles.container}>
+        <ScrollView style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)}
+          />
+          <Button submitTodo={this.submitTodo} />
+        </ScrollView>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor:'#f5f5f5'
+    backgroundColor: "#f5f5f5",
   },
   content: {
     flex: 1,
-    paddingTop: 60
-  }
-})
-
-
-
+    paddingTop: 60,
+  },
+});
